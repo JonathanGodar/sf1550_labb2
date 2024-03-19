99e0f9c77602d18c28b64bb4131fbc5076110da8
Jonathan Niklasson Godar <jonathan.godar@gmail.com>
2024-03-19 10:21:13 +0100

    Copied instructions

---
+++ uppg3.m
@@ -0,0 +1,20 @@
+
+load dollarkurs.mat
+X = USDSEK;
+N = length(X);
+tt=(1:N)';
+
+%% 3a Linjär modell
+
+% Er kod här...
+
+
+%% 3b Linjär + periodisk modell
+
+% Er kod här...
+
+
+%% 3c Icke-linjär modell
+
+% Er kod här...
+
---
+++ uppg4.m
@@ -0,0 +1,77 @@
+
+
+%% 4a Trapetsregeln och Simpson
+
+% Er kod här...
+
+
+%% 4b Konvergensstudier
+
+% Er kod här...
+
+
+%% 4c Trapetsregeln i 2D
+
+% Er kod här...
+
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function V=trapets(n)
+
+%  Indata:
+%
+%  n  - antal delintervall (skalär)
+%
+%  Utdata:
+%
+%  V - volymen (skalär)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Er kod här...
+
+
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function V=simpson(n)
+
+%  Indata:
+%
+%  n  - antal delintervall (skalär)
+%
+%  Utdata:
+%
+%  V - volymen (skalär)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Er kod här...
+
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function V=trapets2d(n)
+
+%  Indata:
+%
+%  n  - antal delintervall i varje koordinatriktning (skalär)
+%
+%  Utdata:
+%
+%  V - volymen (skalär)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Er kod här...
+
+
+end
+
+
