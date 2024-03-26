10e29ffb9df9f090b1ef823016ea843d6d84c699
JonathanGodar <jonathan.godar@gmail.com>
2024-03-26 12:57:06 +0100

    Temp commit before changin stuff

--- uppg3.m
+++ uppg3.m
@@ -3,32 +3,80 @@ load dollarkurs.mat
 X = USDSEK';
 N = length(X);
 tt=(1:N)';
+days = 1:1:N;
 
 %% 3a Linjär modell
 
 % Er kod här...
-day = 1:1:N;
-plot(day, USDSEK)
+
 hold on
-coefficients = interpolate(day, X, 1);
+coefficients = least_square_polynomial(days, X, 1);
 
-models = evaluate_polynomial_at(coefficients', day)
 
-plot(day, X)
-plot(day, )
-error = 
-plot(day, evaluate_polynomial_at(coefficients', day))
+model = evaluate_polynomial_at(coefficients', days)';
+error = model - X;
+
+grid on
+plot(days, X); 
+plot(days, model);
+plot(days, error);
+
+average_square_error = calculate_average_square_error(X,model)
 
 %% 3b Linjär + periodisk modell
+% coefficients = least_square_custom(days, X, 1);
+L = 430;
+f = @(x) [1 x sin(2* pi * x / L) cos(2*pi * x /L)];
+plot_and_check_model(f)
+
+% Perioden ser ut att vara ca 430
 
 % Er kod här...
 
 
 %% 3c Icke-linjär modell
 
-% Er kod här...
 
-function [coeffs] = interpolate(x_points, y_points, grad) 
+function plot_and_check_model(f)
+    coeffs = least_square(days, X, f);
+
+    model = [];
+    for day = days 
+        model = [model, f(day)*coeffs];
+    end
+    % model = f_excplict(days)
+
+    error = model - X;
+
+    grid on
+    plot(days, X);
+    hold on
+    plot(days, model);
+    plot(days, error);
+end
+
+
+function [coeffs] = least_square(x_points, y_points, f) 
+    val_matrix = [];
+    for x_point = x_points  
+        row =  f(x_point) % [1 x_point sin(2* pi * x_point / L) cos(2*pi * x_point /L)]
+        val_matrix = [val_matrix; row];
+    end
+    coeffs = val_matrix\y_points';
+end
+
+% function [coeffs] = least_square_custom(x_points, y_points, grad) 
+%     L = 430;
+%     val_matrix = [];
+%     for x_point = x_points  
+%         row = [];
+%         row = [1 x_point sin(2* pi * x_point / L) cos(2*pi * x_point /L)]
+%         val_matrix = [val_matrix; row];
+%     end
+%     coeffs = val_matrix\y_points';
+% end
+
+function [coeffs] = least_square_polynomial(x_points, y_points, grad) 
     val_matrix = [];
     for x_point = x_points  
         row = [];
@@ -51,12 +99,12 @@ function evaluated = evaluate_polynomial_at(coefficients, x_values)
     end
 end
 
-function average_square_error = calculate_average_square_error (t_value,function_value)
-    N = length(t_value);
+function average_square_error = calculate_average_square_error(actual_value,model_value)
+    N = length(actual_value);
     sum_of_squares = 0;
     for index = (1:N)
-        sum_of_squares = sum_of_squares + (t_value(index) - function_value(index))^2
+        sum_of_squares = sum_of_squares + (actual_value(index) - model_value(index))^2;
     end
 
-    average_square_error = sum_of_squares/N
+    average_square_error = sum_of_squares/N;
 end
