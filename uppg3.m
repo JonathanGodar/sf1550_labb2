e5e5a34bc582b110428388ed271b4e4a4fe194a4
JonathanGodar <jonathan.godar@gmail.com>
2024-03-26 18:53:40 +0100

    Fixed things

--- uppg3.m
+++ uppg3.m
@@ -1,5 +1,4 @@
-
-load dollarkurs.mat
+load dollarkurs.mat;
 U = USDSEK';
 N = length(U);
 tt=(1:N)';
@@ -8,48 +7,54 @@ t = 1:1:N;
 %% 3a Linjär modell
 % Er kod här...
 
-f = @(x) [1 x]
-plot_and_check_model(t, U, f)
-
+figure(1)
+f = @(x) [1 x];
+X = plot_and_check_model(t, U, f);
+disp(["c_0 = ";"c_1 = "] + X);
 
 %% 3b Linjär + periodisk modell
 % coefficients = least_square_custom(t, X, 1);
 % Perioden ser ut att vara ca 430
+figure(2)
 L = 430;
 f = @(x) [1 x sin(2* pi * x / L) cos(2*pi * x /L)];
-X = [plot_and_check_model(t, U, f); L]
+X = [plot_and_check_model(t, U, f); L];
+disp(["d_0 = ";"d_1 = ";"d_2 = ";"d_3 = "; "L = "] + X);
 
 
 %% 3c Icke-linjär modell
 % Er kod här...
 
-Xprev = 10 + X 
-tau = 1e-8
+figure(3)
+Xprev = 10 + X;
+tau = 1e-8;
 
 while norm(Xprev-X) > tau
-    Xprev = X 
+    Xprev = X;
     X = X - (J(X,U)'*J(X,U))^-1*J(X,U)'*F(X,U);
 end
 
 f = @(x) [1 x sin(2* pi * x / L) cos(2*pi * x /L)];
 
-X
+
 model = make_model(X,U);
 
-
-error = model - U;
+error = model' - U;
 grid on
 plot(t, U);
+
 hold on
 plot(t, model);
+
 plot(t, error);
 
+disp("Medelkvadratfelet = " + calculate_average_square_error(model,U));
+disp(["d_0 = ";"d_1 = ";"d_2 = ";"d_3 = ";"L = "] + X);
 
-calculate_average_square_error(model,U)
 %%
 
 function F_vec = F(X, y)
-    F_vec = []
+    F_vec = [];
     for t = 1:size(y,2)
         row = [X(1) + X(2)*t + X(3) * sin(2*pi*t/X(5)) + X(4)*cos(2*pi*t/X(5)) - y(t)];
         F_vec = [F_vec; row];
@@ -57,7 +62,7 @@ function F_vec = F(X, y)
 end
 
 function F_vec = make_model(X, y)
-    F_vec = []
+    F_vec = [];
     for t = 1:size(y,2)
         row = [X(1) + X(2)*t + X(3) * sin(2*pi*t/X(5)) + X(4)*cos(2*pi*t/X(5))];
         F_vec = [F_vec; row];
@@ -66,7 +71,7 @@ end
 
 
 function F_jacobian= J(X, y)
-    F_jacobian = []
+    F_jacobian = [];
     for t = 1:size(y,2)
         row = [1 t  sin(2*pi*t/X(5)) cos(2*pi*t/X(5)) (X(4)*sin(2*pi*t/X(5)) - X(3)*cos(2*pi*t/X(5))) * 2*pi*t/(X(5)^2) ];
         F_jacobian = [F_jacobian; row];
@@ -90,51 +95,18 @@ function coeffs = plot_and_check_model(x_values, y_values, f)
     plot(x_values, model);
     plot(x_values, error);
     
-    calculate_average_square_error(model,y_values)
+    MSE = calculate_average_square_error(model,y_values);
+    disp("Medelkvadratfelet: " + MSE);
 end
 
 
 function [coeffs] = least_square(x_points, y_points, f) 
     val_matrix = [];
     for x_point = x_points  
-        row =  f(x_point) % [1 x_point sin(2* pi * x_point / L) cos(2*pi * x_point /L)]
+        row =  f(x_point);
         val_matrix = [val_matrix; row];
     end
     coeffs = val_matrix\y_points';
-end
-
-% function [coeffs] = least_square_custom(x_points, y_points, grad) 
-%     L = 430;
-%     val_matrix = [];
-%     for x_point = x_points  
-%         row = [];
-%         row = [1 x_point sin(2* pi * x_point / L) cos(2*pi * x_point /L)]
-%         val_matrix = [val_matrix; row];
-%     end
-%     coeffs = val_matrix\y_points';
-% end
-
-function [coeffs] = least_square_polynomial(x_points, y_points, grad) 
-    val_matrix = [];
-    for x_point = x_points  
-        row = [];
-        for exponent = 0:grad
-            row = [row x_point^exponent];
-        end
-        val_matrix = [val_matrix; row];
-    end
-    coeffs = val_matrix\y_points';
-end
-
-function evaluated = evaluate_polynomial_at(coefficients, x_values) 
-    evaluated = [];
-    for x_value = x_values
-        sum = 0;
-        for coeff_idx= [1:size(coefficients,2)]
-            sum = sum + coefficients(coeff_idx) * x_value^(coeff_idx-1);
-        end
-        evaluated = [evaluated; sum];
-    end
 end
 
 function average_square_error = calculate_average_square_error(actual_value,model_value)
--- uppg4.m
+++ uppg4.m
@@ -2,27 +2,90 @@
 
 %% 4a Trapetsregeln och Simpson
 
-% Er kod här...
+format long
+trap_30 = trapets(30)
+trap_60 = trapets(60)
 
+simp_30 = simpson(30)
+simp_60 = simpson(60)
+
+format long
+disp(["intervall\Metod" "Trapets" "Simpson"; 
+			"n = 30:" trap_30 simp_30; 
+			"n = 60:" trap_60 simp_60]);
 
 %% 4b Konvergensstudier
 
 % Er kod här...
+%1
+ref = simpson(1e5);
+err_trap = [];
+x_max = 1000
 
+x_trap = 1:x_max;
+for n = x_trap
+    err_trap = [err_trap abs(ref - trapets(n))];
+end
+
+x_simp = 2:2:x_max;
+err_simp = [];
+for n = x_simp
+    err_simp = [err_simp abs(ref - simpson(n))];
+end
+
+R = 3;
+loglog(R*x_trap.^-1,err_trap);
+hold on;
+loglog(R*x_simp.^-1,err_simp);
+grid on
+
+x_comp_trap = linspace(R/x_trap(1),R/x_trap(end),100);
+y_comp_trap = x_comp_trap.^2 / x_comp_trap(1) * err_trap(1);
+loglog(x_comp_trap, y_comp_trap)
+
+x_comp_simp = linspace(R/x_simp(1),R/x_simp(end),100);
+y_comp_simp = x_comp_simp.^4 / x_comp_simp(1) * err_simp(1);
+loglog(x_comp_simp, y_comp_simp)
+% y1 = x.^3 / sizeList(1)^3 * timeList(1); % +   % timeList(1);
+% y2 = x.^2 / sizeList(1)^2 * timeList(1);
+% y3 = x.^2.475 / sizeList(1)^2.475 * timeList(1);
+
+
+%% 2
+R=3;
+
+series_of_n = 60:2:80
+ratio_trap = [];
+for n = series_of_n 
+    ratio = (trapets(n)-trapets(2*n))/(trapets(2*n)-trapets(4*n));
+    ratio_trap = [ratio_trap; ratio];
+end
+
+ratio_simp = [];
+for n = series_of_n
+    ratio = (simpson(n)-simpson(n*2))/(simpson(n*2)-simpson(n*4));
+    ratio_simp = [ratio_simp; ratio];
+end
+disp(["Trapets" "Simpson"; 
+			ratio_trap ratio_simp]);
 
 %% 4c Trapetsregeln i 2D
 
-% Er kod här...
+trap_20 = trapets2d(1000);
+disp("Volym = " + trap_20)
 
-
-
-
+series_of_n = 60:2:80
+ratio_trap = [];
+for n = series_of_n 
+    ratio = (trapets2d(n)-trapets2d(2*n))/(trapets2d(2*n)-trapets2d(4*n));
+    ratio_trap = [ratio_trap; ratio];
+end
+disp(["Konvergensstudie"; ratio_trap])
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function V=trapets(n)
-
 %  Indata:
 %
 %  n  - antal delintervall (skalär)
@@ -31,17 +94,25 @@ function V=trapets(n)
 %
 %  V - volymen (skalär)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	R = 3;
+	g = @(r) 3* r^3 * exp(-r) / (1 + 1/3 * sin(8*r/5));
 
-% Er kod här...
+	spaced = linspace(0,R,n+1);
+	h = R/(n);
 
-
+	I = 0;
+	for r = spaced(1:end-1)
+				trap = h*(g(r+h)*(r+h)+g(r)*(r))/2;
+				I = I + trap;
+	end
+	V_0 = g(R)*R^2*pi;
+	V =  V_0 - 2*pi*I;
 end
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function V=simpson(n)
-
 %  Indata:
 %
 %  n  - antal delintervall (skalär)
@@ -50,12 +121,26 @@ function V=simpson(n)
 %
 %  V - volymen (skalär)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	R = 3;
+	g = @(r) 3* r^3 * exp(-r) / (1 + 1/3 * sin(8*r/5));
 
-% Er kod här...
+	intervals = linspace(0,R,n+1);
+	h = R/(n);
 
-
+	I = g(0) * 0 + g(R)*R; 
+	for j = 2:(size(intervals,2)-1)
+		coeff = 2;
+		if mod(j,2) == 0 
+			coeff = 4;
+		end
+		
+		I = I + coeff * g(intervals(j)) * intervals(j);
+	end
+	I = h/3 * I;
+    
+	V_0 = g(R)*R^2*pi;
+	V =  V_0 - 2*pi*I;
 end
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function V=trapets2d(n)
@@ -68,10 +153,30 @@ function V=trapets2d(n)
 %
 %  V - volymen (skalär)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	R = 3;
+	g = @(r) 3* r^3 * exp(-r) / (1 + 1/3 * sin(8*r/5));
+	f = @(x,y) g(R) - g(sqrt(x^2+y^2));
 
-% Er kod här...
+	% Er kod här...
+	L = 3 * sqrt(2);
+	x_intervals = linspace(-L/2, L/2, n+1);
+	y_intervals = linspace(-L/2, L/2, n+1);
+	h = L/n;
+	I = 0;
+	for i = 1:length(x_intervals)
+			for j = 1:length(y_intervals) 
+					coeff = 1;
+					if i == 1 || i == length(x_intervals) || j == 1 || j == length(y_intervals)
+							coeff = 1/2;
+							if (i == 1 && j == 1) || (i == 1 && j == length(y_intervals)) || (i == length(x_intervals) && j == 1) || (i == length(x_intervals) && j == length(y_intervals))
+									coeff = 1/4;
+							end
+					end
+					I = I + f(x_intervals(i),y_intervals(j))*coeff*h^2;
+			end
+	end
 
-
+	V = I;
 end
 
 
